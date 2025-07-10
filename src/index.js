38432f6c052b5205aa6b93fdc37830cd1e6091b2
Joshua / Catherine Moylan <35865600+DatSonicBoom@users.noreply.github.com>
2025-07-11 08:17:06 +1000

    Add Express routes

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+app.get("/api", (req, res) => {
+
+    // Create date object with current date
+    let date = new Date();
+
+    // Return results
+    return res.status(200).json({ "unix": date.valueOf(), "utc": date.toUTCString() });
+});
+
+app.get("/api/:date", (req, res) => {
+
+    // Create date variable
+    let date;
+
+    // If the "date" param is one positive integer, construct date with it as an integer
+    if (/^\d+$/.test(req.params.date))
+        date = new Date(parseInt(req.params.date, 10));
+
+    // Otherwise, construct date with the raw "date" param
+    else
+        date = new Date(req.params.date);
+
+    // Return status 400 and error message for invalid dates
+    if (isNaN(date))
+        return res.status(400).json({ "error": "Invalid Date" });
+
+    // Return status 200 and information for valid dates
+    return res.status(200).json({ "unix": date.valueOf(), "utc": date.toUTCString() });
+});
+
+app.listen(3000, () => {
+    console.log("Process listening on port 3000!");
+});
